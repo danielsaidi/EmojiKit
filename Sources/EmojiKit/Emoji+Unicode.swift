f20d5c16ffd8cd8c4bb41ebdb19866109b612a9d
Daniel Saidi <daniel.saidi@gmail.com>
2024-02-22 10:27:49 +0100

    Add emoji unicode name property

--- Sources/EmojiKit/Emoji+Unicode.swift
+++ Sources/EmojiKit/Emoji+Unicode.swift
@@ -10,8 +10,28 @@ import Foundation
 
 public extension Emoji {
     
-    /// The emoji's unique unicode identifier.
+    /// The emoji's unique identifier.
     var unicodeIdentifier: String? {
         char.applyingTransform(.toUnicodeName, reverse: false)
+    }
+    
+    /// The emoji's full, readable unicode name.
+    ///
+    /// Note that this name may not always be what you want to display to users. For that, use the localized name instead.
+    var unicodeName: String {
+        unicodeNameComponents
+            .joined(separator: " ")
+    }
+}
+
+private extension Emoji {
+
+    var unicodeNameComponents: [String] {
+        unicodeIdentifier?
+            .replacingOccurrences(of: "\\N", with: "")
+            .replacingOccurrences(of: "\\n", with: "")
+            .replacingOccurrences(of: "{", with: "")
+            .split(separator: "}")
+            .map { $0.capitalized } ?? []
     }
 }
--- Tests/EmojiKitTests/Emojii+UnicodeTests.swift
+++ Tests/EmojiKitTests/Emojii+UnicodeTests.swift
@@ -15,10 +15,25 @@ final class Emoji_UnicodeTests: XCTestCase {
         Emoji(emoji).unicodeIdentifier
     }
     
+    func name(for emoji: String) -> String? {
+        Emoji(emoji).unicodeName
+    }
+    
     func testUnicodeIdentifierIsRawValue() {
         XCTAssertEqual(id(for: "😀"), "\\N{GRINNING FACE}")
         XCTAssertEqual(id(for: "👍"), "\\N{THUMBS UP SIGN}")
         XCTAssertEqual(id(for: "👍🏿"), "\\N{THUMBS UP SIGN}\\N{EMOJI MODIFIER FITZPATRICK TYPE-6}")
+        XCTAssertEqual(id(for: "👰🏾‍♂️"), "\\N{BRIDE WITH VEIL}\\N{EMOJI MODIFIER FITZPATRICK TYPE-5}\\N{ZERO WIDTH JOINER}\\N{MALE SIGN}\\N{VARIATION SELECTOR-16}")
         XCTAssertEqual(id(for: "👩🏽‍❤️‍👨🏾"), "\\N{WOMAN}\\N{EMOJI MODIFIER FITZPATRICK TYPE-4}\\N{ZERO WIDTH JOINER}\\N{HEAVY BLACK HEART}\\N{VARIATION SELECTOR-16}\\N{ZERO WIDTH JOINER}\\N{MAN}\\N{EMOJI MODIFIER FITZPATRICK TYPE-5}")
+        XCTAssertEqual(id(for: "🧑🏼‍❤️‍💋‍🧑🏿"), "\\N{ADULT}\\N{EMOJI MODIFIER FITZPATRICK TYPE-3}\\N{ZERO WIDTH JOINER}\\N{HEAVY BLACK HEART}\\N{VARIATION SELECTOR-16}\\N{ZERO WIDTH JOINER}\\N{KISS MARK}\\N{ZERO WIDTH JOINER}\\N{ADULT}\\N{EMOJI MODIFIER FITZPATRICK TYPE-6}")
+    }
+    
+    func testUnicodeNameIsFormattedUnicodeIdentifier() {
+        XCTAssertEqual(name(for: "😀"), "Grinning Face")
+        XCTAssertEqual(name(for: "👍"), "Thumbs Up Sign")
+        XCTAssertEqual(name(for: "👍🏿"), "Thumbs Up Sign Emoji Modifier Fitzpatrick Type-6")
+        XCTAssertEqual(name(for: "👰🏾‍♂️"), "BRIDE WITH VEIL EMOJI MODIFIER FITZPATRICK TYPE-5 ZERO WIDTH JOINER MALE SIGN VARIATION SELECTOR-16".capitalized)
+        XCTAssertEqual(name(for: "👩🏽‍❤️‍👨🏾"), "WOMAN EMOJI MODIFIER FITZPATRICK TYPE-4 ZERO WIDTH JOINER HEAVY BLACK HEART VARIATION SELECTOR-16 ZERO WIDTH JOINER MAN EMOJI MODIFIER FITZPATRICK TYPE-5".capitalized)
+        XCTAssertEqual(name(for: "🧑🏼‍❤️‍💋‍🧑🏿"), "ADULT EMOJI MODIFIER FITZPATRICK TYPE-3 ZERO WIDTH JOINER HEAVY BLACK HEART VARIATION SELECTOR-16 ZERO WIDTH JOINER KISS MARK ZERO WIDTH JOINER ADULT EMOJI MODIFIER FITZPATRICK TYPE-6".capitalized)
     }
 }
