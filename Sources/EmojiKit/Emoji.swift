8bc0455ae48db532c4e96747524c1a9b0eee2081
Daniel Saidi <daniel.saidi@gmail.com>
2024-02-22 09:27:53 +0100

    Add emoji struct

---
+++ Sources/EmojiKit/Emoji.swift
@@ -0,0 +1,35 @@
+//
+//  Emoji.swift
+//  EmojiKit
+//
+//  Created by Daniel Saidi on 2021-01-17.
+//  Copyright © 2021-2024 Daniel Saidi. All rights reserved.
+//
+
+import Foundation
+
+/**
+ This type represents an emoji and serves as a namespace for
+ emoji-related types and functionality.
+ */
+public struct Emoji: Equatable, Codable, Identifiable {
+    
+    /// Create an emoji from a certain character.
+    public init(_ char: Character) {
+        self.char = String(char)
+    }
+    
+    /// Create an emoji from a certain string.
+    public init(_ char: String) {
+        self.char = char
+    }
+    
+    /// The emoji character string.
+    public let char: String
+}
+
+public extension Emoji {
+    
+    /// The emoji's unique identifier.
+    var id: String { char }
+}
---
+++ Tests/EmojiKitTests/EmojiTests.swift
@@ -0,0 +1,37 @@
+//
+//  EmojiTests.swift
+//  EmojiKitTests
+//
+//  Created by Daniel Saidi on 2023-10-31.
+//  Copyright © 2023-2024 Daniel Saidi. All rights reserved.
+//
+
+import EmojiKit
+import XCTest
+
+final class EmojiTests: XCTestCase {
+
+    func testCanBeCreatedWithCharacter() {
+        let emoji = Emoji(Character("😀"))
+        XCTAssertEqual(emoji.char, "😀")
+    }
+    
+    func testCanBeCreatedWithString() {
+        let emoji = Emoji("🤭")
+        XCTAssertEqual(emoji.char, "🤭")
+    }
+    
+    func testIdentifierIsUnique() {
+        let emoji1 = Emoji("👍")
+        let emoji2 = Emoji("👍🏿")
+        XCTAssertEqual(emoji1.id, "👍")
+        XCTAssertNotEqual(emoji1.id, "👍🏿")
+        XCTAssertEqual(emoji2.id, "👍🏿")
+        XCTAssertNotEqual(emoji2.id, "👍🏽")
+    }
+}
+
+private extension String {
+
+    var char: Character { Array(self)[0] }
+}
