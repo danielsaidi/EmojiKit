f33a1428a66be4b8df255ff3db143594891831d2
Daniel Saidi <daniel.saidi@gmail.com>
2024-02-23 10:53:32 +0100

    Add skin tone support

---
+++ Tests/EmojiKitTests/Emoji+SkinToneTests.swift
@@ -0,0 +1,52 @@
+//
+//  Emoji+SkinToneTests.swift
+//  EmojiKitTests
+//
+//  Created by Daniel Saidi on 2023-11-01.
+//  Copyright © 2023-2024 Daniel Saidi. All rights reserved.
+//
+
+import EmojiKit
+import XCTest
+
+final class Emoji_SkinToneTests: XCTestCase {
+    
+    func hasVariants(_ emoji: String) -> Bool {
+        Emoji(emoji).hasSkinToneVariants
+    }
+    
+    func neutralVariant(for emoji: String) -> String {
+        Emoji(emoji).neutralSkinToneVariant.char
+    }
+    
+    func variants(for emoji: String) -> String {
+        Emoji(emoji).skinToneVariants
+            .map { $0.char }
+            .joined()
+    }
+
+    func testHasSkinToneVariantsIsTrueForSomeEmojis() {
+        XCTAssertTrue(hasVariants("👍"))
+        XCTAssertFalse(hasVariants("🚀"))
+    }
+
+    func testNeutralSkinToneVariantIsDefinedForSomeEmojis() {
+        XCTAssertEqual(neutralVariant(for: "👍"), "👍")
+        XCTAssertEqual(neutralVariant(for: "👍🏿"), "👍")
+        XCTAssertEqual(neutralVariant(for: "👨🏻‍🚒"), "👨‍🚒")
+    }
+
+    func testNeutralSkinToneVariantIsSameForSomeEmojis() throws {
+        XCTAssertEqual(neutralVariant(for: "🚀"), "🚀")
+    }
+
+    func testSkinToneVariantIsDefinedForSomeEmojis() {
+        XCTAssertEqual(variants(for: "👍"), "👍👍🏻👍🏼👍🏽👍🏾👍🏿")
+        XCTAssertEqual(variants(for: "👍🏿"), "👍👍🏻👍🏼👍🏽👍🏾👍🏿")
+        XCTAssertEqual(variants(for: "👨🏻‍🚒"), "👨‍🚒👨🏻‍🚒👨🏼‍🚒👨🏽‍🚒👨🏾‍🚒👨🏿‍🚒")
+    }
+
+    func testSkinToneVariantIsEmptyForSomeEmojis() throws {
+        XCTAssertEqual(variants(for: "🚀").count, 0)
+    }
+}
