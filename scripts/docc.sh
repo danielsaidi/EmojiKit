760781da9c69f74871acaffe16de06d41ba6a07f
Daniel Saidi <daniel.saidi@gmail.com>
2024-10-04 01:46:06 +0200

    Add missing redirect to DocC script

--- scripts/docc.sh
+++ scripts/docc.sh
@@ -4,6 +4,9 @@
 
 # USAGE: bash scripts/docc.sh <TARGET>
 
+TARGET=$1
+TARGET_LOWERCASED=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+
 swift package resolve;
 
 xcodebuild docbuild -scheme $1 -derivedDataPath /tmp/docbuild -destination 'generic/platform=iOS';
@@ -12,3 +15,5 @@ $(xcrun --find docc) process-archive \
 transform-for-static-hosting /tmp/docbuild/Build/Products/Debug-iphoneos/$1.doccarchive \
 --output-path .build/docs \
 --hosting-base-path '$1';
+
+echo "<script>window.location.href += \"/documentation/$TARGET_LOWERCASED\"</script>" > .build/docs/index.html;
