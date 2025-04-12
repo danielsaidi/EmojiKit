29c2e07034fe6a0f5f5359289a206b247cc33638
Aidar Galimov <aidar.gf@gmail.com>
2025-04-13 01:04:18 +0500

    Add Italian localization

--- Tests/EmojiKitTests/Localization/EmojiCategory+LocalizationTests.swift
+++ Tests/EmojiKitTests/Localization/EmojiCategory+LocalizationTests.swift
@@ -77,6 +77,18 @@ final class EmojiCategory_LocalizationTests: XCTestCase {
         XCTAssertEqual(name(for: .flags, locale: locale), "Flaggen")
     }
 
+    func testLocalizedNameIsAvailableInItalian() {
+        let locale = Locale.italian
+        XCTAssertEqual(name(for: .smileysAndPeople, locale: locale), "Faccine & Persone")
+        XCTAssertEqual(name(for: .animalsAndNature, locale: locale), "Animali & Natura")
+        XCTAssertEqual(name(for: .foodAndDrink, locale: locale), "Cibo & Bevande")
+        XCTAssertEqual(name(for: .activity, locale: locale), "Attività")
+        XCTAssertEqual(name(for: .travelAndPlaces, locale: locale), "Viaggi & Luoghi")
+        XCTAssertEqual(name(for: .objects, locale: locale), "Oggetti")
+        XCTAssertEqual(name(for: .symbols, locale: locale), "Simboli")
+        XCTAssertEqual(name(for: .flags, locale: locale), "Bandiere")
+    }
+
     func testLocalizedNameHasFallbackForUnsupportedLocales() {
         let emoji = Emoji("😀")
         XCTAssertEqual(
